537862a657aaaf8dff276e6a5e3da870e3c80055
SKyzjBpN <sensitivestory@gmail.com>
2021-05-13 23:06:04 +0300

    Adding functionality to handle attention requests

--- mockdbhelper.py
+++ mockdbhelper.py
@@ -1,9 +1,13 @@
+import datetime
+
+
 MOCK_USERS = [{"email": "test@example.com", "salt": "pLlgTRYlE11EGGhuofj/ZJqU/CQ=",
               "hashed": "4a2ae72127911c2842b1b1f4835c0a6567b18f7f4e1d62bdb7f84953d4f068476d1048fcf0758a9243db7e2c29c69e"
                         "47788572b3e1ffd3a10dcf3bdb6303aaff"}]
 
-
 MOCK_TABLES = [{"_id": "1", "number": "1", "owner": "test@example.com", "url": "mockurl"}]
+
+MOCK_REQUESTS = [{"_id": "1", "table_number": "1","table_id": "1", "time": datetime.datetime.now()}]
 
 
 class MockDBHelper:
@@ -17,7 +21,7 @@ class MockDBHelper:
         MOCK_USERS.append({"email": email, "salt": salt, "hashed": hashed})
 
     def add_table(self, number, owner):
-        MOCK_TABLES.append({"_id": number, "number": number, "owner": owner})
+        MOCK_TABLES.append({"_id": str(number), "number": number, "owner": owner})
         return number
 
     def update_table(self, _id, url):
@@ -34,3 +38,12 @@ class MockDBHelper:
             if table.get("_id") == table_id:
                 del MOCK_TABLES[i]
             break
+
+    def get_requests(self, owner_id):
+        return MOCK_REQUESTS
+
+    def delete_request(self, request_id):
+        for i, request in enumerate(MOCK_REQUESTS):
+            if request.get("_id") == request_id:
+                del MOCK_REQUESTS[i]
+                break
--- waitercaller.py
+++ waitercaller.py
@@ -1,4 +1,4 @@
-#import datetime
+import datetime
 
 from flask import Flask
 from flask import redirect
@@ -68,10 +68,30 @@ def account_createtable():
     return redirect(url_for('account'))
 
 
+@app.route("/newrequest/<tid>")
+def new_request(tid):
+    DB.add_request(tid, datetime.datetime.now())
+    return "Your request has been logged and a waiter will be with you shortly"
+
+
 @app.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template("dashboard.html")
+    now = datetime.datetime.now().replace(microsecond=0)
+    requests = DB.get_requests(current_user.get_id())
+    for req in requests:
+        deltaseconds = (now - req['time']).seconds
+        req['wait_minutes'] = "{}.{}".format((deltaseconds/60), str(deltaseconds % 60).zfill(2))
+
+    return render_template("dashboard.html", requests=requests)
+
+
+@app.route("/dashboard/resolve")
+@login_required
+def dashboard_resolve():
+    request_id = request.args.get("request_id")
+    DB.delete_request(request_id)
+    return redirect(url_for('dashboard'))
 
 
 @app.route("/logout")

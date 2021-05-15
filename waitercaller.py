bf360d7ef4b35b08342e53c596632acc08c13cd2
SKyzjBpN <sensitivestory@gmail.com>
2021-05-15 18:36:24 +0300

    Adding WTForms

--- waitercaller.py
+++ waitercaller.py
@@ -17,6 +17,9 @@ from passwordhelper import PasswordHelper
 from bitlyhelper import BitlyHelper
 from user import User
 
+from forms import CreateTableForm
+from forms import LoginForm
+from forms import RegistrationForm
 
 import config
 
@@ -40,14 +43,14 @@ def load_user(user_id):
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", loginform=LoginForm(), registrationform=RegistrationForm())
 
 
 @app.route("/account")
 @login_required
 def account():
     tables = DB.get_tables(current_user.get_id())
-    return render_template("account.html", tables=tables)
+    return render_template("account.html", createtableform=CreateTableForm(), tables=tables)
 
 
 @app.route("/account/deletetable")
@@ -61,11 +64,13 @@ def account_deletetable():
 @app.route("/account/createtable", methods=["POST"])
 @login_required
 def account_createtable():
-    tablename = request.form.get("tablenumber")
-    tableid = DB.add_table(tablename, current_user.get_id())
-    new_url = BH.shorten_url(config.base_url + "newrequest/" + tableid)
-    DB.update_table(tableid, new_url)
-    return redirect(url_for('account'))
+    form = CreateTableForm(request.form)
+    if form.validate():
+        tableid = DB.add_table(form.tablenumber.data, current_user.get_id())
+        new_url = BH.shorten_url(config.base_url + "newrequest/" + tableid)
+        DB.update_table(tableid, new_url)
+        return redirect(url_for('account'))
+    return render_template("account.html", createtableform=form, tables=DB.get_tables(current_user.get_id()))
 
 
 @app.route("/newrequest/<tid>")
@@ -102,29 +107,30 @@ def logout():
 
 @app.route("/login", methods=["POST"])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    stored_user = DB.get_user(email)
-    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
-        user = User(email)
-        login_user(user)
-        return redirect(url_for('account'))
-    return home()
+    form = LoginForm(request.form)
+    if form.validate():
+        stored_user = DB.get_user(form.loginemail.data)
+        if stored_user and PH.validate_password(form.loginpassword.data, stored_user['salt'], stored_user['hashed']):
+            user = User(form.loginemail.data)
+            login_user(user, remember=True)
+            return redirect(url_for('account'))
+        form.loginemail.errors.append("Email or password invalid")
+    return render_template("home.html", loginform=form, registrationform=RegistrationForm())
 
 
 @app.route("/register", methods=["POST"])
 def register():
-    email = request.form.get("email")
-    pw1 = request.form.get("password")
-    pw2 = request.form.get("password2")
-    if not pw1 == pw2:
-        return redirect(url_for("home"))
-    if DB.get_user(email):
-        return redirect(url_for("home"))
-    salt = PH.get_salt()
-    hashed = PH.get_hash(pw1+salt)
-    DB.add_user(email, salt, hashed)
-    return redirect(url_for("home"))
+    form = RegistrationForm(request.form)
+    if form.validate():
+        if DB.get_user(form.email.data):
+            form.email.errors.append("Email address already registered")
+            return render_template("home.html", loginform=LoginForm(), registrationform=form)
+        salt = PH.get_salt()
+        hashed = PH.get_hash(form.password2.data + salt)
+        DB.add_user(form.email.data, salt, hashed)
+        return render_template("home.html", loginform=LoginForm(), registrationform=form,
+                               onloadmessage="Registration successful. Please log in.")
+    return render_template("home.html", loginform=LoginForm(), registrationform=form)
 
 
 if __name__ == '__main__':

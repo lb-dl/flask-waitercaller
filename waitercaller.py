f7a9c909d89b072df026fba8a8764b8d7529db90
SKyzjBpN <sensitivestory@gmail.com>
2021-05-16 17:24:19 +0300

    Adding MongoDB

--- waitercaller.py
+++ waitercaller.py
@@ -23,7 +23,10 @@ from forms import RegistrationForm
 
 import config
 
-
+if config.test:
+    from mockdbhelper import MockDBHelper as DBHelper
+else:
+    from dbhelper import DBHelper
 DB = DBHelper()
 PH = PasswordHelper()
 BH = BitlyHelper()
@@ -67,7 +70,7 @@ def account_createtable():
     form = CreateTableForm(request.form)
     if form.validate():
         tableid = DB.add_table(form.tablenumber.data, current_user.get_id())
-        new_url = BH.shorten_url(config.base_url + "newrequest/" + tableid)
+        new_url = BH.shorten_url(config.base_url + "newrequest/" + str(tableid))
         DB.update_table(tableid, new_url)
         return redirect(url_for('account'))
     return render_template("account.html", createtableform=form, tables=DB.get_tables(current_user.get_id()))
